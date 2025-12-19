9f8b8e25febc37cd03ae6e057603addd9c28777d
Ayush Kewadia <ayush.kewadia@gmail.com>
2025-12-19 14:57:19 +0530

    Advance JS4
    Into to async

---
+++ Cohot2.0/Frontend/Adv JS/JS4/4.1/script.js
@@ -0,0 +1,4 @@
+// JS -> Single threaded lang hai means ek kaam ek baar mai kar paayegi, Line by line code chalega, It is synchronus approach
+
+// Asynchrnous Appoarch -> jo kaam turant ho sakta hai usse turant kardo, aur jo kaam mai time lagega uss kaam ko side mai chalate raho aur jab wo kaam ho jaaye tab kardo
+
