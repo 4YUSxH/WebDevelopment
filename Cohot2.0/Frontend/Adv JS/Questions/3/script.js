641a3280944c56db0eba3e0fa7cc6a21e09b99a4
Ayush Kewadia <ayush.kewadia@gmail.com>
2025-12-20 20:04:10 +0530

    Question3

--- Cohot2.0/Frontend/Adv JS/Questions/3/script.js
+++ Cohot2.0/Frontend/Adv JS/Questions/3/script.js
@@ -7,14 +7,14 @@ let button = document.querySelector("button");
 let error = document.querySelector(".error");
 let cityInput = document.querySelector("#cityInput");
 
-let apikey = "1a5e016e1555d7e0478cbad542bca7c0";
+let apikey = "";
 
 button.addEventListener("click", () => {
   async function print(city) {
     let raw = await fetch( // await code ka executiion stop kar dega jab tak data nahi aata api se
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
     );
-    let finalOp = await raw.json(); // await jab tak data nahi aata api se cause raw ke upar hi json() karana hai hence raw is required
+    let finalOp = await raw.json(); // await jab tak data nahi aata api se cause raw ke upar hi json() karana hai hence raw is require
 
     try{
         if(finalOp.cod == 404){
