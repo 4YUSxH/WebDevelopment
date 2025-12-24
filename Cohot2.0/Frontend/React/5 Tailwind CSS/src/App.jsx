4fa7c2a12a2d35922d4c4e16e6347eabfc165b0d
Ayush Kewadia <ayush.kewadia@gmail.com>
2025-12-24 17:37:09 +0530

    5 Tailwind CSS

---
+++ Cohot2.0/Frontend/React/5 Tailwind CSS/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold ">I am stlyed with Tailwind Css :)</h1>
+      <div className="h-100 w-100 bg-amber-700"></div>
+      <div className="h-[150px] w-[100vw] bg-pink-700">
+        For giving custom vlues use []
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+// Tailwind css is more customizable than bootstrap cause we are making our compnoents from scratch unlike bootsrap has its own premade components those are less customizable
+// Tailwind css load fast and it is mobile first framework hence responsive
