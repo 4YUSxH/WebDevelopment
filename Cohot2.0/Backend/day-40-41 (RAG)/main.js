521bfb1ca65cf24b25e9e357e0c7b1c0fe79aef0
Ayush Kewadia <ayush.kewadia@gmail.com>
2026-09-18 17:28:21 +0530

    Day-40-41 (RAG)

--- Cohot2.0/Backend/day-40-41 (RAG)/main.js
+++ Cohot2.0/Backend/day-40-41 (RAG)/main.js
@@ -1,19 +1,58 @@
+import "dotenv/config";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
+import { Pinecone } from "@pinecone-database/pinecone";
 
-const loader = new PDFLoader("./story.pdf")
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
+const index = pc.index("cohort-2-rag");
 
-const docs = await loader.load()
+const loader = new PDFLoader("./story.pdf");
 
-// console.log(docs);
+const data = await loader.load();
+
+const embeddings = new MistralAIEmbeddings({
+  apiKey: process.env.MISTRAL_API_KEY,
+  model: "mistral-embed",
+});
 
 const spiltter = new RecursiveCharacterTextSplitter({
-    chunkSize: 500,
-    chunkOverlap: 0
-})
+  chunkSize: 500,
+  chunkOverlap: 0,
+});
 
-const chunks = await spiltter.splitDocuments(docs)
+const chunks = await spiltter.splitDocuments(data);
 
-console.log("Number of chunks:", chunks.length);
+const docs = await Promise.all(
+  chunks.map(async (chunk) => {
+    const embeds = await embeddings.embedQuery(chunk.pageContent);
+    return {
+      text: chunk.pageContent,
+      embeds,
+    };
+  }),
+);
 
-console.log(chunks);
+const result = await index.upsert({
+  records: docs.map((doc, i) => ({
+    id: `doc-${i}`,
+    values: doc.embeds,
+    metadata: {
+      text: doc.text,
+    },
+  })),
+});
+
+const queryEmbedding = await embeddings.embedQuery(
+  "how was the arav internship experience",
+);
+
+console.log(queryEmbedding);
+
+const finalResult = await index.query({
+  vector: queryEmbedding,
+  topK: 1,
+  includeMetadata: true,
+});
+
+console.log(JSON.stringify(finalResult));
